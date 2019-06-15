77c5aeda2199c5045561424ce178f47bffa81367
Anuraj R <a4anurajr@gmail.com>
2019-06-15 12:08:18 +0530

    commented the unwanted loop

--- app/src/main/java/com/anuraj/newsapi/MainActivity.java
+++ app/src/main/java/com/anuraj/newsapi/MainActivity.java
@@ -87,16 +87,6 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
