1aeee8a282282530cc97418c12d95280007cd240
Anuraj R <a4anurajr@gmail.com>
2019-06-17 16:25:38 +0530

    Added the App Constants

---
+++ app/src/main/java/com/anuraj/newsapi/Util/Constants.java
@@ -0,0 +1,21 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 17/6/19 3:21 PM
+ *
+ */
+
+package com.anuraj.newsapi.Util;
+
+public interface Constants {
+    String API_KEY = "8190df9eb51445228e397e4185311a66";
+    String NEWS_SOURCE = "bbc-news";
+    String KEY_AUTHOR = "author";
+    String KEY_TITLE = "title";
+    String KEY_DESCRIPTION = "description";
+    String KEY_URL = "url";
+    String KEY_URLTOIMAGE = "urlToImage";
+    String KEY_PUBLISHEDAT = "publishedAt";
+    String NEWS_API = "https://newsapi.org/v2/";
+
+}
