7b98aaf4c2139e308f245d8e3609636e927c1cb4
Anuraj R <a4anurajr@gmail.com>
2019-06-17 18:41:05 +0530

    Added the webview Activity

---
+++ app/src/main/java/com/anuraj/newsapi/ViewActivity/WebViewActivity.java
@@ -0,0 +1,11 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 17/6/19 6:40 PM
+ *
+ */
+
+package com.anuraj.newsapi.ViewActivity;
+
+public class WebViewActivity {
+}
